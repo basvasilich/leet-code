2b1807bc890371945dcc954be574493d2e66ceab
Vadim Patsev <basvasilich@yandex.ru>
2020-04-17 13:00:23 +0300

    https://leetcode.com/problems/container-with-most-water/

--- 11.py
+++ 11.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/container-with-most-water/
+from typing import List
+
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
@@ -21,4 +23,3 @@ class Solution:
                 pointer_1 += 1
 
         return max_area
-
