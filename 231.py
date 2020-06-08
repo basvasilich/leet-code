1d27ae81505abd0cc24f21dd33055b19da179932
Vadim Patsev <basvasilich@yandex.ru>
2020-06-08 20:58:58 +0300

    https://leetcode.com/problems/power-of-two/

---
+++ 231.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        while n > 2:
+            if n % 2 == 1:
+                return False
+            n = n // 2
+        return True
