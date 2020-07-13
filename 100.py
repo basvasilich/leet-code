fcd322afcabc8018f33e6cb1bd48f5e45b0286aa
Vadim Patsev <basvasilich@yandex-team.ru>
2020-07-13 17:17:45 +0300

    https://leetcode.com/problems/same-tree/

--- 100.py
+++ 100.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         def dfsPreOrder(root, result):
