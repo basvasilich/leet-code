a1d5ffd7b0073cd76a118f4f2fdc6d4923a50a52
Vadim Patsev <basvasilich@yandex.ru>
2020-07-17 23:25:34 +0300

    https://leetcode.com/problems/top-k-frequent-elements/

---
+++ 347.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from heapq import heappush, heappop
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        c = Counter(nums)
+        h = []
+        result = []
+
+        for key in c.keys():
+            heappush(h, (-1 * c[key], key))
+
+        while k > 0 and len(h) > 0:
+            value, key = heappop(h)
+            result.append(key)
+            k -= 1
+
+        return result
